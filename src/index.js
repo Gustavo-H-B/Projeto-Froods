e4ad90570bb0e8c17f1336437f781475e3819255
GUSTAVO HENRIQUE BRUESKY <gustavo_bruesky@estudante.sesisenai.org.br>
2026-02-03 19:54:35 -0300

    primeiras rotas

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@
 
 require('dotenv').config();
 
-const express = require('express');
-const app = express();
+const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
